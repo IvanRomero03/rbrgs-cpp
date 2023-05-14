662dcd690dc37f11d6171892085cac01bf6c5233
IvanRomero03 <i.wells.ar@gmail.com>
2023-05-14 16:12:40 -0600

    template

---
+++ template.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <limits.h>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stack>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <utility>
+#include <vector>
+
+#define PI 3.141592653589793238462643383279502884197169399375105820974944592307816406286
+#define EPS 1e-6
+#define INF 1000000000
+#define LLINF 1000000000000000000LL
+#define MOD 1000000007
+#define MAXN 1000005
+
+#define sync ios_base::sync_with_stdio(0), cin.tie(0), cin.tie(0), cout.tie(0), cout.precision(15)
+#define pb push_back
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+
+int main()
+{
+    sync;
+
+    return 0;
+}
