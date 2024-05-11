e8fc80b40cda74b8ac47b38d119bd85ffe8ea20c
Alejandra Coeto <109093534+Ale-Coeto@users.noreply.github.com>
2024-05-11 10:08:58 -0600

    Create FloydWarshall.cpp

---
+++ Algorithms/Graph/FloydWarshall.cpp
@@ -0,0 +1,80 @@
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <deque>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <limits.h>
+#include <map>
+#include <math.h>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <utility>
+#include <vector>
+ 
+#define PI 3.141592653589793
+#define EPS 0.000000001
+// #define INF 1000000000
+ 
+#define _ ios_base::sync_with_stdio(0), cin.tie(0), cin.tie(0), cout.tie(0), cout.precision(15);
+#define FOR(i, a, b) for(int i=int(a); i<int(b); i++)
+#define RFOR(i, a, b) for(int i=int(a)-1; i>=int(b); i--)
+#define FORC(cont, it) for(typeof((cont).begin()) it = (cont).begin(); it != (cont).end(); it++)
+#define RFORC(cont, it) for(typeof((cont).rbegin()) it = (cont).rbegin(); it != (cont).rend(); it++)
+#define pb push_back
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ll> vl;
+typedef vector<ii> vii;
+ 
+#define MAXN 400
+#define MOD 1000000007
+
+// O(n^3) - max_size = 400 vertices
+// All pairs shortest paths
+
+
+ll INF = LLONG_MAX;
+vector<vl> dp(MAXN, vl(MAXN, INF)); //Update MAX to max size
+
+// For vertices from 1-n
+void floydWarshall(int n) {
+    for (int k = 1; k <= n; k++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (dp[i][k] == INF || dp[k][j] == INF) continue;
+                dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j]);
+            }
+        }
+    }
+}
+ 
+int main() { _
+
+    int m,a,b;
+    ll w;
+    cin >> m;
+
+    // Fill dp with edges and then do floydWarshall
+    while (m--) {
+        cin >> a >> b >> w;
+    
+        dp[a][b] = min(dp[a][b],w);
+        dp[b][a] = min(dp[b][a],w);
+    }
+
+    return 0;
+}
