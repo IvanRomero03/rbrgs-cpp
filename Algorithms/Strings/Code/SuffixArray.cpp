5b8f6ffa87521a328fa71039be5ffcd6c1ef30c3
IvanRomero03 <i.wells.ar@gmail.com>
2024-05-11 09:52:31 -0600

    suffix array copiado

---
+++ Algorithms/Strings/Code/SuffixArray.cpp
@@ -0,0 +1,96 @@
+//Rbrgs.cpp
+#include <bits/stdc++.h>
+
+#define MOD 1000000007
+#define MAXN 1000005
+
+#define sync ios_base::sync_with_stdio(0), cin.tie(0), cin.tie(0), cout.tie(0), cout.precision(15)
+#define aFor(i, a) for (auto &i : a)
+#define pb push_back
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+
+vector<int> sort_cyclic_shifts(string const& s) {
+    int n = s.size();
+    const int alphabet = 256;
+    vector<int> p(n), c(n), cnt(max(alphabet, n), 0);
+    for (int i = 0; i < n; i++)
+        cnt[s[i]]++;
+    for (int i = 1; i < alphabet; i++)
+        cnt[i] += cnt[i-1];
+    for (int i = 0; i < n; i++)
+        p[--cnt[s[i]]] = i;
+    c[p[0]] = 0;
+    int classes = 1;
+    for (int i = 1; i < n; i++) {
+        if (s[p[i]] != s[p[i-1]])
+            classes++;
+        c[p[i]] = classes - 1;
+    }
+    vector<int> pn(n), cn(n);
+    for (int h = 0; (1 << h) < n; ++h) {
+        for (int i = 0; i < n; i++) {
+            pn[i] = p[i] - (1 << h);
+            if (pn[i] < 0)
+                pn[i] += n;
+        }
+        fill(cnt.begin(), cnt.begin() + classes, 0);
+        for (int i = 0; i < n; i++)
+            cnt[c[pn[i]]]++;
+        for (int i = 1; i < classes; i++)
+            cnt[i] += cnt[i-1];
+        for (int i = n-1; i >= 0; i--)
+            p[--cnt[c[pn[i]]]] = pn[i];
+        cn[p[0]] = 0;
+        classes = 1;
+        for (int i = 1; i < n; i++) {
+            pair<int, int> cur = {c[p[i]], c[(p[i] + (1 << h)) % n]};
+            pair<int, int> prev = {c[p[i-1]], c[(p[i-1] + (1 << h)) % n]};
+            if (cur != prev)
+                ++classes;
+            cn[p[i]] = classes - 1;
+        }
+        c.swap(cn);
+    }
+    return p;
+}
+
+vector<int> suffix_array_construction(string s) {
+    s += "$";
+    vector<int> sorted_shifts = sort_cyclic_shifts(s);
+    sorted_shifts.erase(sorted_shifts.begin());
+    return sorted_shifts;
+}
+
+int compare(int i, int j, int l, int k) {
+    pair<int, int> a = {c[k][i], c[k][(i+l-(1 << k))%n]};
+    pair<int, int> b = {c[k][j], c[k][(j+l-(1 << k))%n]};
+    return a == b ? 0 : a < b ? -1 : 1;
+}
+
+int lcp(int i, int j) {
+    int ans = 0;
+    for (int k = log_n; k >= 0; k--) {
+        if (c[k][i % n] == c[k][j % n]) {
+            ans += 1 << k;
+            i += 1 << k;
+            j += 1 << k;
+        }
+    }
+    return ans;
+}
+
+
+int main()
+{
+    sync;
+
+    return 0;
+}
